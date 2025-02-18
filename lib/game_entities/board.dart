137a868c3182ae6446ee6916cdaea99670a24e34
Jhalakupadhyay <jhalak92upadhyay@gmail.com>
2025-02-18 23:45:34 +0530

    feat: added the dynamic view of the board in the game screen.

--- lib/game_entities/board.dart
+++ lib/game_entities/board.dart
@@ -1,10 +1,20 @@
 import 'package:flame/components.dart';
+import 'package:jungle_game/airhockey_game.dart';
 
-class Board extends SpriteComponent{
-  
+class Board extends SpriteComponent with HasGameRef<AirHockeyGame> {
+  @override
   Future<void> onLoad() async {
-    sprite = await Sprite.load('assets/game_entities/BOARD.png');
-    size = Vector2(400, 200);
-    position = Vector2(50, 100);
+    sprite = await Sprite.load('BOARD.png');
+    
+    final boardWidth = game.size.x * 0.8;
+    const aspectRatio = 1600 / 900;
+    final boardHeight = boardWidth * aspectRatio;
+    
+    size = Vector2(boardWidth, boardHeight);
+    anchor = Anchor.center;
+    
+    // Center the board on screen
+    position = Vector2(0, 0);
   }
+  
 }
